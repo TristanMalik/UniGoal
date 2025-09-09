bae643ef76c19778e5c973b3cc3d30c8bea3e121
TristanMalik <anthrax.drakkar@gmail.com>
2025-09-10 00:11:34 +0700

    Checkpoints Completed

--- main/models.py
+++ main/models.py
@@ -11,22 +11,9 @@ class News(models.Model):
         ('analysis', 'Analysis'),
     ]
     
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField(default=0)
+    description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    news_views = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
     is_featured = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return self.title
-    
-    @property
-    def is_news_hot(self):
-        return self.news_views > 20
-        
-    def increment_views(self):
-        self.news_views += 1
-        self.save()
